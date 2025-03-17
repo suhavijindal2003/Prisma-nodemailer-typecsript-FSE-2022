4489a235abaff2c3ad388bac086071fe2e278edc
suhavijindal2003 <suhavi.jindal03@gmail.com>
2025-03-17 15:35:13 +0530

    add files

--- react/React/src/App.jsx
+++ react/React/src/App.jsx
@@ -4,23 +4,51 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
+
+
+
 function Increase(){
   setCount(count+1)
 }
 
 
-const [bgColor, setBgColor] = useState("#ffffff"); // Initial background color
+const [bgColor, setBgColor] = useState("#ffffff"); 
 
 function generateRandomColor() {
-  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  setBgColor(randomColor);
+  let bgColor=["red","pink","brown","black","yellow","green","silver" ,"purple","magenta","blue"];
+  let index=Math.floor(Math.random()*bgColor.length);
+  setBgColor(bgColor[index]);
+
 }
+const [value,setValue]=useState(0);
+function changevalue(){
+  let index=Math.floor(Math.random()*10);
+  setValue(index);
+}
+
+
 
   return (
     <>
       {/* <Counter/> */}
 
-      <div style={{ backgroundColor: bgColor, height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+     
+        <div Style={{height:"50vh",width:"20vh",display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",backgroundColor:"pink"}}>
+          <h1>{value}</h1>
+          {/* {value===7?<img src="https://tse3.mm.bing.net/th?id=OIP.7pAr9s31kcPT0ix_tru_GQHaHa&pid=Api&P=0&h=180"></img>:<img src='https://tse4.mm.bing.net/th?id=OIP.K2trbkXTI2GgQD4y1bR4_AHaHP&pid=Api&P=0&h=180'></img>} */}
+        {/* {value==7 && <h1>you win</h1>} */}
+        {value==7 || <h1>you lose</h1>}
+        <button  onClick={changevalue}
+        style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer", border: "none", borderRadius: "5px", backgroundColor: "#000", color: "#fff" }}
+      >
+        Click
+      </button>
+     
+
+        </div>
+      
+
+      <div style={{ backgroundColor: bgColor, height: "50vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
       <h1>Random Background Color</h1>
       <button 
         onClick={generateRandomColor} 
